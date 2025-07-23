3c86e9013957a472ff104fc58a22010f8702a316
dollydasheep <MelPoPo21@gmail.com>
2025-07-23 14:31:55 +0800

    update dashboard, added button on navbar

--- src/app/dashboard/dashboard.tsx
+++ src/app/dashboard/dashboard.tsx
@@ -3,8 +3,18 @@ import Navbar from "./navbar";
 
 export default function Dashboard(){
     return(
-        <div className="border border-gray-300 w-full md:w-2/3 h-2/3">
+        <div className="relative border border-gray-300 w-full md:w-2/3 h-2/3">
             <Navbar />
+            <div className="m-4 w-[calc(100%-2em)] h-[calc(100%-5.5em)] grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 auto-rows-min gap-2">
+                <div className="flex flex-col justify-center w-full h-24 border border-gray-300 rounded-lg hover:border-violet-500 cursor-pointer shadow-sm">
+                    <span className="ml-[10%]">
+                        <h1 className="text-xl text-left font-medium">Hair Comb</h1>
+                        <p className="text-xs text-left">Just a piece of hair comb</p>
+                        <p>$200</p>
+                    </span>
+                </div>
+
+            </div>
         </div>
     )
 }
--- src/app/dashboard/navbar.tsx
+++ src/app/dashboard/navbar.tsx
@@ -1,10 +1,16 @@
-import { Bell } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Bell, Plus } from "lucide-react";
 
 
 export default function Navbar(){
     return(
         <div className="w-full h-14 bg-violet-600 z-50 flex justify-between items-center shadow-md">
-            <div className="flex text-white space-x-8 ml-8">
+            <div className="flex items-center text-white space-x-4   ml-8">
+                <Button className="bg-violet-600 hover:bg-violet-500 cursor-pointer" asChild>
+                    <span>
+                        <Plus />
+                    </span>
+                </Button>
                 <p className="underline-animation hover:scale-110 hover:font-medium transition duration-200 cursor-pointer">Items</p>
                 <p className="underline-animation hover:scale-110 hover:font-medium transition duration-200 cursor-pointer">Trade</p>
             </div>
